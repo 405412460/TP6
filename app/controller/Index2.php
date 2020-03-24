383ae8d9c1653d8ce0ed808b25d5043613b70445
wuweifeng <405412460@qq.com>
2020-03-24 12:53:03 +0800

    1

---
+++ app/controller/Index2.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\controller;
+
+use app\BaseController;
+
+class Index2 extends BaseController
+{
+    public function index()
+    {
+        return 1;
+    }
+
+    public function hello()
+    {
+        return 'hello,';
+    }
+}
